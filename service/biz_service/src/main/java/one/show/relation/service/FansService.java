fbafd3843ea6bce221fb671c9bf19daa6a8a447b
vilizhe <82569673@qq.com>
2018-05-08 14:51:07 +0800

    粉丝落户

--- service/biz_service/src/main/java/one/show/relation/service/FansService.java
+++ service/biz_service/src/main/java/one/show/relation/service/FansService.java
@@ -14,12 +14,26 @@ import one.show.relation.domain.Fans;
  */
 public interface FansService {
 	
+	/**
+	 * 根据uid获取用户粉丝列表
+	 */
 	public List<Fans> findListByUid(Long uid, Integer start, Integer count) throws ServiceException;
 	
+	
+	/**
+	 * 保存用户粉丝
+	 */
 	public void save(Fans fans) throws ServiceException;
 	
+	
+	/**
+	 * 删除粉丝
+	 */
 	public void delete(Long uid, Long fid) throws ServiceException;
 
+	/**
+	 * 获取粉丝数
+	 */
 	public Integer findFansCountByUid(Long uid) throws ServiceException;
 }
 
