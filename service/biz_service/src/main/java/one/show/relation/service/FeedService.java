34b40fa7d26922d59ecaffbd55c3df03d0d9ba61
vilizhe <82569673@qq.com>
2018-05-09 17:02:35 +0800

    更新推送接口

--- service/biz_service/src/main/java/one/show/relation/service/FeedService.java
+++ service/biz_service/src/main/java/one/show/relation/service/FeedService.java
@@ -15,12 +15,42 @@ import one.show.relation.domain.Feed;
  */
 public interface FeedService {
 	
+	/**
+	 * 根据uid获取推送列表
+	 * @param uid
+	 * @param condition
+	 * @param start
+	 * @param count
+	 * @return
+	 * @throws ServiceException
+	 */
 	public List<Feed> findListByUid(Long uid, Map<String, String> condition, Integer start, Integer count) throws ServiceException;
 	
+	
+	/**
+	 * 保存推送数据
+	 * @param feed
+	 * @throws ServiceException
+	 */
 	public void save(Feed feed) throws ServiceException;
 	
+	/**
+	 * 删除
+	 * @param uid
+	 * @param fid  关注人ID
+	 * @param point 推送类型
+	 * @throws ServiceException
+	 */
 	public void delete(Long uid, Long fid, String point) throws ServiceException;
 	
+	/**
+	 * 更新
+	 * @param uid
+	 * @param fid
+	 * @param point
+	 * @param updateContent
+	 * @throws ServiceException
+	 */
 	public void update(Long uid, Long fid, String point, Map<String, String> updateContent) throws ServiceException;
 
 }
