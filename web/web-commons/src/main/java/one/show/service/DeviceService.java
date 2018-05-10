2bc2f85f261b86028061526d8be005606241d228
hank <hank@show.one>
2018-05-10 23:59:16 +0800

    add device

--- web/web-commons/src/main/java/one/show/service/DeviceService.java
+++ web/web-commons/src/main/java/one/show/service/DeviceService.java
@@ -10,7 +10,19 @@ import one.show.user.thrift.view.DeviceView;
 import one.show.user.thrift.view.UserDeviceView;
 
 public interface DeviceService {
+	/**
+	 * 根据id查询设备
+	 * @param did
+	 * @return
+	 * @throws ServiceException
+	 */
 	public DeviceView findDeviceByDid(String did) throws ServiceException;
+	/**
+	 * 查询设备信息
+	 * @param did
+	 * @param params
+	 * @throws ServiceException
+	 */
 	public void updateDevice(String did,Map<String,String> params) throws ServiceException;
 	
 	/**
@@ -21,6 +33,11 @@ public interface DeviceService {
 	 */
 	public boolean deviceIsForbidden(String did) throws ServiceException;
 	
+	/**
+	 * 保存设备封禁信息
+	 * @param deviceForbiddenView
+	 * @throws ServiceException
+	 */
 	public void saveForbidden(DeviceForbiddenView deviceForbiddenView) throws ServiceException;
 	
 	public void deleteForbidden(String did) throws ServiceException;
