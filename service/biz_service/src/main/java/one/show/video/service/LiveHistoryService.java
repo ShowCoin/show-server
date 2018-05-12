de05b2325a2fb44457f40bbfb43ebf05100406e4
sunke <sunke@show.one>
2018-05-13 00:12:20 +0800

    fix note live history

--- service/biz_service/src/main/java/one/show/video/service/LiveHistoryService.java
+++ service/biz_service/src/main/java/one/show/video/service/LiveHistoryService.java
@@ -8,12 +8,34 @@ import one.show.video.domain.LiveHistory;
 
 public interface LiveHistoryService {
 	
+	/**
+	 * 保存直播回放
+	 * @param liveHistory
+	 * @throws ServiceException
+	 */
 	public void save(LiveHistory liveHistory) throws ServiceException;
 	
+	/**
+	 * 根据ids获得直播回放列表
+	 * @param ids
+	 * @return
+	 */
 	public List<LiveHistory> findLiveHistoryListByIds(List<Long> ids);
 	
+	/**
+	 * 根据直播ID获得回放
+	 * @param liveId
+	 * @return
+	 * @throws ServiceException
+	 */
 	public LiveHistory findLiveHistoryListById(Long liveId) throws ServiceException;
 
+	/**
+	 * 更新直播信息
+	 * @param liveid
+	 * @param updateContent
+	 * @throws ServiceException
+	 */
 	public void update(long liveid,Map<String, String> updateContent) throws ServiceException;
 	
 }
