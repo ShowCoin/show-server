eceee40f7070683988a5299c4b77fec606397e9b
sunke <sunke@show.one>
2018-05-15 20:00:57 +0800

    fix LiverecordService note

--- service/biz_service/src/main/java/one/show/video/service/LiveRecordService.java
+++ service/biz_service/src/main/java/one/show/video/service/LiveRecordService.java
@@ -8,20 +8,69 @@ import org.apache.ibatis.annotations.Param;
 
 public interface LiveRecordService {
 
+	/**
+	 * 根据ID获得直播记录
+	 * 
+	 * @param id
+	 * @return
+	 */
 	public LiveRecord findById(long id);
 
+	/**
+	 * 根据处理ID获得直播记录列表
+	 * 
+	 * @param persistentId
+	 * @return
+	 */
 	public List<LiveRecord> findByPersistentId(String persistentId);
-	
-	public List<LiveRecord> findByLiveid(long liveid);
-	
-	public List<LiveRecord> findByStatus( int status);
 
+	/**
+	 * 根据直播ID获得直播记录列表
+	 * 
+	 * @param liveid
+	 * @return
+	 */
+	public List<LiveRecord> findByLiveid(long liveid);
+
+	/**
+	 * 根据直播状态获得直播记录列表
+	 * 
+	 * @param status
+	 * @return
+	 */
+	public List<LiveRecord> findByStatus(int status);
+
+	/**
+	 * 保存直播记录
+	 * 
+	 * @param record
+	 * @return
+	 */
 	public int save(LiveRecord record);
 
+	/**
+	 * 根据直播ID更新直播状态
+	 * 
+	 * @param liveid
+	 * @param status
+	 */
 	void updateStatusByLiveId(long liveid, int status);
-	
-	void updatePersistentIdByLiveId(long liveid, int format,String persistentId, int status);
-	
+
+	/**
+	 * 根据直播ID更新格式、处理ID、状态
+	 * 
+	 * @param liveid
+	 * @param format
+	 * @param persistentId
+	 * @param status
+	 */
+	void updatePersistentIdByLiveId(long liveid, int format, String persistentId, int status);
+
+	/**
+	 * 根据ID删除直播记录
+	 * 
+	 * @param id
+	 */
 	void deleteLiveRecord(long id);
 
 }
