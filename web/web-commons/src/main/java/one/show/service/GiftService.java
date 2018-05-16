250d1ec494e46524561b1adf621959bf08909afa
sunke <sunke@show.one>
2018-05-17 00:22:07 +0800

    fix gift note

--- web/web-commons/src/main/java/one/show/service/GiftService.java
+++ web/web-commons/src/main/java/one/show/service/GiftService.java
@@ -19,16 +19,53 @@ import one.show.manage.thrift.view.GiftView;
  */
 public interface GiftService {
 	
+	/**
+	 * 获得礼物列表
+	 * @param giftType
+	 * @param status
+	 * @param start
+	 * @param count
+	 * @return
+	 * @throws ServiceException
+	 */
 	public GiftListView getGiftList(Integer giftType, ADMIN_STATUS status, int start, int count) throws ServiceException; 
 
+	/**
+	 * 保存礼物
+	 * @param giftView
+	 * @throws ServiceException
+	 */
 	public void save(GiftView giftView) throws ServiceException;
 	
+	/**
+	 * 更新礼物
+	 * @param giftId
+	 * @param updateContent
+	 * @throws ServiceException
+	 */
 	public void update(Integer giftId, Map<String, String> updateContent) throws ServiceException;
 	
+	/**
+	 * 删除礼物
+	 * @param giftId
+	 * @throws ServiceException
+	 */
 	public void delete(Integer giftId) throws ServiceException;
 	
+	/**
+	 * 根据状态获得礼物列表
+	 * @param status
+	 * @return
+	 * @throws ServiceException
+	 */
 	public List<GiftTypeView> getGiftTypeList(ADMIN_STATUS status) throws ServiceException;
 	
+	/**
+	 * 根据ID获得礼物
+	 * @param giftId
+	 * @return
+	 * @throws ServiceException
+	 */
 	public GiftView getGiftById(Integer giftId) throws ServiceException;
 	
 	
