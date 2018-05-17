46a7073bba68e5d1936613648122868e489ae668
hank <hank@show.one>
2018-05-18 01:52:27 +0800

    add rank comment

--- web/web-commons/src/main/java/one/show/service/RankService.java
+++ web/web-commons/src/main/java/one/show/service/RankService.java
@@ -8,12 +8,30 @@ import one.show.common.exception.ServiceException;
 import one.show.stat.thrift.view.RankUserListView;
 
 /**
- * Created by Andy on 18/4/13.
+ * Created by Andy on 18/4/16.
  */
 public interface RankService {
+	/**
+	 * 获取用户排名列表
+	 * @param beginTime
+	 * @param endTime
+	 * @param type
+	 * @param start
+	 * @param count
+	 * @return
+	 * @throws ServiceException
+	 */
 	public RankUserListView findUserRankList(int beginTime, int endTime,
 			int type, int start, int count) throws ServiceException;
 	
+	/**
+	 * 获取守护者列表
+	 * @param uid
+	 * @param start
+	 * @param count
+	 * @return
+	 * @throws ServiceException
+	 */
 	public List<Map<String, Object>> findDefenderList(final long uid, final int start, final int count) throws ServiceException;
 
 }
