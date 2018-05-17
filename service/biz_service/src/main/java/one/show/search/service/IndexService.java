0b41d4ab469966eee70aadf7959adad26b2e94e9
vilizhe <82569673@qq.com>
2018-05-17 12:06:14 +0800

    search

--- service/biz_service/src/main/java/one/show/search/service/IndexService.java
+++ service/biz_service/src/main/java/one/show/search/service/IndexService.java
@@ -4,6 +4,12 @@ import one.show.common.exception.ServiceException;
 
 public interface IndexService {
 	
+	
+	/**
+	 * 创建索引文件
+	 * @param indexPath 索引文件位置
+	 * @throws ServiceException
+	 */
 	public void createUserIndex(String indexPath) throws ServiceException;
 	
 }
