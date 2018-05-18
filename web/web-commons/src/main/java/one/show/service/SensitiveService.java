4527599a420247c12778de11344e6132efb01567
hank <hank@show.one>
2018-05-18 23:47:48 +0800

    sensitivewords

--- web/web-commons/src/main/java/one/show/service/SensitiveService.java
+++ web/web-commons/src/main/java/one/show/service/SensitiveService.java
@@ -22,14 +22,43 @@ public interface SensitiveService {
 	 * @throws ServiceException
 	 */
 	public String filter(String word,int appId) throws ServiceException;
-	
+	/**
+	 * 根据appid获取列表
+	 * @param appId
+	 * @param word
+	 * @param start
+	 * @param count
+	 * @return
+	 * @throws ServiceException
+	 */
 	public WordListView findListByAppId(Integer appId, String word, Integer start, Integer count) throws ServiceException;
 
+	/**
+	 * 保存敏感词
+	 * @param wordView
+	 * @throws ServiceException
+	 */
 	public void save(WordView wordView) throws ServiceException;
 	
+	/**
+	 * 删除敏感词
+	 * @param id
+	 * @throws ServiceException
+	 */
 	public void delete(Integer id) throws ServiceException;
 	
+	/**
+	 * 修改敏感词
+	 * @param id
+	 * @param word
+	 * @throws ServiceException
+	 */
 	public void update(Integer id, String word) throws ServiceException;
 	
+	/**
+	 * 获取所有app
+	 * @return
+	 * @throws ServiceException
+	 */
 	public List<AppView> findAppList() throws ServiceException;
 }
