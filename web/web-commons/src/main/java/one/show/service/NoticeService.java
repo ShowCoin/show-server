612411f4084d8e8aaa4d78c21e51e4c6e553e678
hank <hank@show.one>
2018-05-18 20:35:34 +0800

    update note func

--- web/web-commons/src/main/java/one/show/service/NoticeService.java
+++ web/web-commons/src/main/java/one/show/service/NoticeService.java
@@ -17,17 +17,50 @@ import one.show.manage.thrift.view.NoticeView;
  */
 public interface NoticeService {
 	
+	/**
+	 * 获取通知列表
+	 * @return
+	 * @throws ServiceException
+	 */
 	public List<NoticeView> getEffecNoticeList() throws ServiceException; 
 	
+	/**
+	 * 分页获取通知列表
+	 * @param start
+	 * @param count
+	 * @return
+	 * @throws ServiceException
+	 */
 	public NoticeListView getNoticeList(int start, int count) throws ServiceException; 
 
+	/**
+	 * 保存通知
+	 * @param noticeView
+	 * @throws ServiceException
+	 */
 	public void save(NoticeView noticeView) throws ServiceException;
 	
+	/**
+	 * 更新通知
+	 * @param id
+	 * @param updateContent
+	 * @throws ServiceException
+	 */
 	public void update(Integer id, Map<String, String> updateContent) throws ServiceException;
 	
+	/**
+	 * 删除通知
+	 * @param id
+	 * @throws ServiceException
+	 */
 	public void delete(Integer id) throws ServiceException;
 	
-	
+	/**
+	 * 根据id获取通知
+	 * @param id
+	 * @return
+	 * @throws ServiceException
+	 */
 	public NoticeView getNoticeById(Integer id) throws ServiceException;
 	
 	
