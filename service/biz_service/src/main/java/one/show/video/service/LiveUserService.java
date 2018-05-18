05933df62f43af94b86023b44e3d6d34ca13df98
sunke <sunke@show.one>
2018-05-18 14:20:28 +0800

    fix

--- service/biz_service/src/main/java/one/show/video/service/LiveUserService.java
+++ service/biz_service/src/main/java/one/show/video/service/LiveUserService.java
@@ -8,13 +8,49 @@ import one.show.video.domain.ReturnList;
 
 public interface LiveUserService {
 	
+	/**
+	 * 保存直播用户
+	 * @param liveUser
+	 * @throws ServiceException
+	 */
 	public void save(LiveUser liveUser) throws ServiceException;
 	
+	/**
+	 * 根据UID获得直播列表
+	 * @param uid
+	 * @param sort
+	 * @param condition
+	 * @param start
+	 * @param count
+	 * @return
+	 * @throws ServiceException
+	 */
 	public ReturnList<LiveUser> getLiveListByUid(Long uid, String sort,Map<String, String> condition, Integer start, Integer count) throws ServiceException;
 	
+	/**
+	 * 根据UID获得直播数量
+	 * @param uid
+	 * @param condition
+	 * @return
+	 * @throws ServiceException
+	 */
 	public Integer getLiveCountByUid(Long uid, Map<String, String> condition) throws ServiceException;
 
+	/**
+	 * 根据uid和liveId获得直播用户
+	 * @param uid
+	 * @param liveid
+	 * @return
+	 * @throws ServiceException
+	 */
 	public LiveUser findLiveUser(long uid,long liveid) throws ServiceException;
 	
+	/**
+	 * 更新直播用户
+	 * @param uid
+	 * @param liveId
+	 * @param updateContent
+	 * @throws ServiceException
+	 */
 	public void updateLiveUser(long uid, long liveId,Map<String, String> updateContent) throws ServiceException;
 }
