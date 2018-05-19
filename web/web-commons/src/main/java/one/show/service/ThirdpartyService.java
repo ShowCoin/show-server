2466c660e04c2a5e9bf936040c1223d7b7c39f76
hank <hank@show.one>
2018-05-19 23:18:33 +0800

    userInfo

--- web/web-commons/src/main/java/one/show/service/ThirdpartyService.java
+++ web/web-commons/src/main/java/one/show/service/ThirdpartyService.java
@@ -8,6 +8,12 @@ import java.util.List;
  * Created by Haliaeetus leucocephalus on 18/1/6.
  */
 public interface ThirdpartyService {
+	/**
+	 * 获取用户信息
+	 * @param id
+	 * @param token
+	 * @return
+	 */
     public UserView getUserInfo(String id,  String token);
     
     public <T> List<T> getFoucsList(String id, String token) throws Exception;
