9c9394fa1627ebce023da814459ecbc67f7e5827
vilizhe <82569673@qq.com>
2018-05-21 20:23:55 +0800

    c

--- service/biz_service/src/main/java/one/show/user/service/WhiteListService.java
+++ service/biz_service/src/main/java/one/show/user/service/WhiteListService.java
@@ -9,12 +9,37 @@ import one.show.user.domain.WhiteList;
  * Created by kevin on 15/8/19.
  */
 public interface WhiteListService {
+	/**
+	 * 保存白名单
+	 * 白名单中的用户直播不需要审核
+	 * @param uid
+	 * @param time
+	 * @throws ServiceException
+	 */
     void save(long uid, int time) throws ServiceException;
 
+    /**
+     * 把用户从白名单中删除
+     * @param uid
+     * @throws ServiceException
+     */
     void remove(long uid) throws ServiceException;
 
+    /**
+     * 获取白名单列表
+     * @param cursor 开始行号
+     * @param count	总行数
+     * @return
+     * @throws ServiceException
+     */
     List<WhiteList> findWhiteList(int cursor, int count) throws ServiceException;
 
+    /**
+     * 根据uid获取白名单数据
+     * @param uid
+     * @return
+     * @throws ServiceException
+     */
     WhiteList findWhiteListByUid(long uid) throws ServiceException;
     
    
