662305a38a1b56c9c2be031515ed9f0798269bc7
vilizhe <82569673@qq.com>
2018-05-23 18:01:53 +0800

    Version Control

--- service/biz_service/src/main/java/one/show/manage/service/VersionControlService.java
+++ service/biz_service/src/main/java/one/show/manage/service/VersionControlService.java
@@ -4,5 +4,11 @@ import one.show.common.exception.ServiceException;
 import one.show.manage.domain.VersionControl;
 
 public interface VersionControlService {
+	/**
+	 * 获取版本更新信息
+	 * @param agentType 0android  1ios
+	 * @return
+	 * @throws ServiceException
+	 */
 	public VersionControl findVersionControl(int agentType)throws ServiceException;
 }
