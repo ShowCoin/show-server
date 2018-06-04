be39e5a26ac56c534b8a77a37b19b5d5c55cdecb
sunke <sunke@show.one>
2018-06-04 15:29:30 +0800

    fix note

--- service/biz_service/src/main/java/one/show/manage/service/LevelService.java
+++ service/biz_service/src/main/java/one/show/manage/service/LevelService.java
@@ -6,9 +6,25 @@ import one.show.common.exception.ServiceException;
 import one.show.manage.domain.FanLevel;
 import one.show.manage.domain.MasterLevel;
 
+/**
+ * 等级服务
+ *
+ */
 public interface LevelService {
-	
+
+	/**
+	 * 主播等级列表
+	 * 
+	 * @return
+	 * @throws ServiceException
+	 */
 	public List<MasterLevel> findMasterLevelList() throws ServiceException;
-	
+
+	/**
+	 * 粉丝等级列表
+	 * 
+	 * @return
+	 * @throws ServiceException
+	 */
 	public List<FanLevel> findFanLevelList() throws ServiceException;
 }
