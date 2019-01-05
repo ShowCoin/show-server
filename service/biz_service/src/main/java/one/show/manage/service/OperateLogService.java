a5dd0e703aa35f44048e029ecbfdd2b7b7aafe47
kdso-sc <liuxiaolei@show.one>
2019-01-05 19:01:22 +0800

    operate log

--- service/biz_service/src/main/java/one/show/manage/service/OperateLogService.java
+++ service/biz_service/src/main/java/one/show/manage/service/OperateLogService.java
@@ -15,6 +15,8 @@ public interface OperateLogService {
 	public void save(OperateLog operateLog) throws ServiceException;
 
 	public void find(Long uid) throws ServiceException;
+
+	public void update(OperateLog operateLog) throws ServiceException;
 	
 }
 
